486380e4c71f5ebf0e98db4f6f7c8a683c714083
Luca Realdi <lr@opencontent.it>
2016-01-17 11:41:09 +0100

    call js from template

--- settings/design.ini.append.php
+++ settings/design.ini.append.php
@@ -5,7 +5,7 @@ DesignExtensions[]=ezflowplayer
 
 [JavaScriptSettings]
 # List of JavaScript files to include in pagelayout
-JavaScriptList[]=flowplayer-3.0.6.min.js
+#JavaScriptList[]=flowplayer-3.0.6.min.js
 
 [StylesheetSettings]
 CSSFileList[]=flowplayer.css
